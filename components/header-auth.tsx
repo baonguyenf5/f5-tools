ce4227626ea29e5413760ff9bac80c068144b9f3
Bao <bao.nguyen@gigarion.com>
2025-04-16 21:55:01 +0700

    add login with mic

--- components/header-auth.tsx
+++ components/header-auth.tsx
@@ -34,15 +34,6 @@ export default async function AuthButton() {
             >
               <Link href="/sign-in">Sign in</Link>
             </Button>
-            <Button
-              asChild
-              size="sm"
-              variant={"default"}
-              disabled
-              className="opacity-75 cursor-none pointer-events-none"
-            >
-              <Link href="/sign-up">Sign up</Link>
-            </Button>
           </div>
         </div>
       </>
@@ -59,11 +50,8 @@ export default async function AuthButton() {
     </div>
   ) : (
     <div className="flex gap-2">
-      <Button asChild size="sm" variant={"outline"}>
-        <Link href="/sign-in">Sign in</Link>
-      </Button>
       <Button asChild size="sm" variant={"default"}>
-        <Link href="/sign-up">Sign up</Link>
+        <Link href="/sign-in">Sign in</Link>
       </Button>
     </div>
   );
